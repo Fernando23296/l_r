dcc09667f3207de1318e240455173f21da8aed9f
fernando23296 <fernando23296@gmail.com>
2020-01-06 10:13:46 -0400

    adding binding

--- r_programming/week1_2.R
+++ r_programming/week1_2.R
@@ -3,3 +3,33 @@
 lista1 <- list(1,"a",TRUE,1+4i)
 print(lista1)
 
+#MATRICES
+#Las matrices son vectores con una dimension de atributo
+m <- matrix(nrow=2,ncol=3)
+print(m)
+print(dim(m))
+print(attributes(m))
+
+#Las matrices pueden ser creadas a partir de un rango
+m2<-matrix(1:6,nrow=2,ncol=3)
+print(m2)
+
+m21<-matrix(1:6,ncol=3,nrow=2)
+print(m21)
+
+#Pueden ser creadas a partir de un vector, añandiendole el atributo de dimension
+m <- 1:10
+print(m)
+dim(m)<-c(2,5)
+print(m)
+
+# Tambien podemos crear matrices uniendolos(binding) usando cbind y rbind
+x <- 1:3
+y <- 10:12
+con_c <- cbind(x,y)
+print(con_c) #c es vertical
+
+con_r <- rbind(x,y)
+print(con_r) #r es horizontal
+
+ 
